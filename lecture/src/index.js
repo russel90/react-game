fc01c7ce72f105e58e48c9b262d673a4b0705f1f
russel <Yongwan.Kim90@gmail.com>
2019-05-20 23:05:48 +0900

    Add WebPack

---
+++ lecture/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import {hot} from "react-hot-loader/root";
+
+// import WordRelay from "./wordRelay";
+// const Hot = hot(WordRelay);
+
+const title = "React Webpack Setup";
+
+ReactDOM.render(
+    <div>
+        {title}
+    </div>,
+    document.getElementById('root')
+);
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,3 +1,45 @@
+const path = require('path');
+
+// webpack 실행 방법: npx webpack
 module.exports = {
-    
+    name: 'word-relay-setting',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval', // 실서비스: hidden-source-map
+    resolve:{
+        extensions: ['*', '.js', '.jsx']
+    },
+    entry:{
+        entry: './src/index.js',
+    }, // 입력
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: '/node_modules/',
+            loader: 'babel-loader',
+            options:{
+                presets:[
+                   ['@babel/preset-env', {
+                        targets:{
+                            browsers: ["> 5% in KR"], // browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react'
+                ],
+                plugins: [
+                    "@babel/plugin-proposal-class-properties",
+                    "react-hot-loader/babel"
+                ],
+            }
+        }]
+    },
+    plugins:[],
+    output: {
+        publicPath: './dist',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+      },
+      devServer: {
+        contentBase: './dist'
+      }
 };
